8a136a9cc62cd34e6a7f07880d02451d0dd1c62d
Barun Parruck <barun.parruck@gmail.com>
2023-06-24 16:18:56 +0100

    Save settings

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,5 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,5 +1,20 @@
 return {
   -- Leaving this here as an example of a plugin for now.
+  lsp = {
+    setup_handlers = {
+      -- add custom handler
+      rust_analyzer = function(_, opts) require("rust-tools").setup { server = opts } end,
+    },
+  },
+  {
+    "simrat39/rust-tools.nvim", -- add lsp plugin
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = { "rust_analyzer" },
+      },
+    },
+  },
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
